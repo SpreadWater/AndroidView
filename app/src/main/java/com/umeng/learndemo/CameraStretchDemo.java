321cfea0b14dbebdea432fcfcca0c5349456fc74
SpreadWater <875737147@qq.com>
2020-09-08 23:43:09 +0800

    9月8日 视图动画，扫描按钮动画学习

---
+++ app/src/main/java/com/umeng/learndemo/CameraStretchDemo.java
@@ -0,0 +1,65 @@
+package com.umeng.learndemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.view.animation.BounceInterpolator;
+import android.view.animation.LinearInterpolator;
+import android.view.animation.RotateAnimation;
+import android.view.animation.ScaleAnimation;
+import android.widget.ImageView;
+
+public class CameraStretchDemo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.scnner);
+        //---------9月8日图片弹簧伸缩demo
+
+//        ImageView imageView=(ImageView)findViewById(R.id.img);
+////        ScaleAnimation scaleAnimation=new ScaleAnimation(1.0f,1.2f,1.0f,1.2f, Animation.RELATIVE_TO_SELF,0.5f,
+////                Animation.RELATIVE_TO_SELF,0.5f);
+////        scaleAnimation.setDuration(6000);
+////        scaleAnimation.setFillAfter(true);
+////        scaleAnimation.setInterpolator(new BounceInterpolator());
+////        imageView.startAnimation(scaleAnimation);
+
+        //------------9月8日加载框效果
+//        ImageView imageView=(ImageView)findViewById(R.id.loading);
+//        RotateAnimation rotateAnim=new RotateAnimation(0,360,Animation.RELATIVE_TO_SELF,0.5f,
+//                Animation.RELATIVE_TO_SELF,0.5f);
+//        rotateAnim.setRepeatCount(Animation.INFINITE);//一直重复
+//        rotateAnim.setDuration(2000);
+//        rotateAnim.setInterpolator(new LinearInterpolator());//匀速旋转
+//        imageView.startAnimation(rotateAnim);
+
+        //------------9月8日扫描动画，点击时，imageview间隔做动画
+        final  Animation animation1= AnimationUtils.loadAnimation(CameraStretchDemo.this,R.anim.scale_alpha_anim);
+        final  Animation animation2= AnimationUtils.loadAnimation(CameraStretchDemo.this,R.anim.scale_alpha_anim);
+        final  Animation animation3= AnimationUtils.loadAnimation(CameraStretchDemo.this,R.anim.scale_alpha_anim);
+        final  Animation animation4= AnimationUtils.loadAnimation(CameraStretchDemo.this,R.anim.scale_alpha_anim);
+
+
+        final ImageView circle1=(ImageView)findViewById(R.id.cicle1);
+        final ImageView circle2=(ImageView)findViewById(R.id.cicle2);
+        final ImageView circle3=(ImageView)findViewById(R.id.cicle3);
+        final ImageView circle4=(ImageView)findViewById(R.id.cicle4);
+
+        findViewById(R.id.start_can).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                circle1.startAnimation(animation1);
+                animation2.setStartOffset(600);
+                circle2.startAnimation(animation2);
+                animation3.setStartOffset(1200);
+                circle3.startAnimation(animation3);
+                animation4.setStartOffset(1800);
+                circle4.startAnimation(animation4);
+            }
+        });
+    }
+}
--- app/src/main/java/com/umeng/learndemo/MainActivity.java
+++ app/src/main/java/com/umeng/learndemo/MainActivity.java
@@ -1,20 +1,99 @@
 package com.umeng.learndemo;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
-import android.widget.Button;
+import android.view.animation.AccelerateDecelerateInterpolator;
+import android.view.animation.AccelerateInterpolator;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.view.animation.AnimationSet;
+import android.view.animation.AnimationUtils;
+import android.view.animation.AnticipateInterpolator;
+import android.view.animation.AnticipateOvershootInterpolator;
+import android.view.animation.BounceInterpolator;
+import android.view.animation.CycleInterpolator;
+import android.view.animation.DecelerateInterpolator;
+import android.view.animation.LinearInterpolator;
+import android.view.animation.RotateAnimation;
+import android.view.animation.ScaleAnimation;
+import android.view.animation.TranslateAnimation;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
-
-import com.umeng.learndemo.view.WaterWaveView;
-
+    //9月8日动画学习demo
 public class MainActivity extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        findViewById(R.id.btn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final TextView tv=(TextView)findViewById(R.id.tv);
+                //--------->9月8日动画学习demo伸缩变化的类
+//                ScaleAnimation animation= new ScaleAnimation(0.0f,1.4f,0.0f,1.4f,
+//                        Animation.RELATIVE_TO_SELF,0.5f,Animation.RELATIVE_TO_SELF,0.5f);
+//                animation.setDuration(700);
+                //--------->9月8日动画学习demo透明度类
+//                AlphaAnimation animation=new AlphaAnimation(1.0f,0.1f);
+//                animation.setDuration(3000);
+//                animation.setFillBefore(true);
+                //--------->9月8日动画学习demo旋转类
+//                RotateAnimation animation=new RotateAnimation(0,-650,
+//                        AlphaAnimation.RELATIVE_TO_SELF,0.5f,AlphaAnimation.RELATIVE_TO_SELF,0.5f);
+//                animation.setDuration(3000);
+//                animation.setFillAfter(true);
+                //--------->9月8日动画学习demo移动类
+//                TranslateAnimation animation=new TranslateAnimation(Animation.ABSOLUTE,0,Animation.ABSOLUTE,-80,
+////                        Animation.ABSOLUTE,0,Animation.ABSOLUTE,-80);
+////                animation.setDuration(2000);
+////                animation.setFillBefore(true);
+                //--------->9月8日动画学习demo组合类
+//                Animation aplha_Anim=new AlphaAnimation(1.0f,0.1f);
+////                Animation scale_Anim=new ScaleAnimation(0.0f,1.4f,0.0f,1.4f,Animation.RELATIVE_TO_SELF,0.5f
+////                ,Animation.RELATIVE_TO_SELF,0.5f);
+////                Animation roate_Anim=new RotateAnimation(0,720,Animation.RELATIVE_TO_SELF,0.5f,Animation.RELATIVE_TO_SELF,0.5f);
+////                AnimationSet setAnim=new AnimationSet(true);//公用一个插值器
+////                setAnim.addAnimation(aplha_Anim);
+////                setAnim.addAnimation(scale_Anim);
+////                setAnim.addAnimation(roate_Anim);
+////                setAnim.setDuration(3000);
+////                setAnim.setFillAfter(true);
 
+                ////--------->9月8日动画学习demo实现动画的监听事件
+//                final RotateAnimation rotateAnim=new RotateAnimation(0,-650,Animation.RELATIVE_TO_SELF,0.5f,
+//                        Animation.RELATIVE_TO_SELF,0.5f);
+//                rotateAnim.setDuration(3000);
+//                rotateAnim.setFillAfter(true);
+//
+//                ScaleAnimation scaleAnim=new ScaleAnimation(0.0f,1.4f,0.0f,1.4f,Animation.RELATIVE_TO_SELF,0.5f,
+//                        Animation.RELATIVE_TO_SELF,0.5f);
+//                scaleAnim.setDuration(700);
+//                scaleAnim.setAnimationListener(new Animation.AnimationListener() {
+//                    @Override
+//                    public void onAnimationStart(Animation animation) {
+//
+//                    }
+//
+//                    @Override
+//                    public void onAnimationEnd(Animation animation) {
+//                            tv.startAnimation(rotateAnim);
+//                    }
+//
+//                    @Override
+//                    public void onAnimationRepeat(Animation animation) {
+//
+//                    }
+//                });
+//                AlphaAnimation animation=new AlphaAnimation(1.0f,0.1f);
+//                animation.setDuration(3000);
+//                animation.setFillBefore(true);
+//                animation.setInterpolator(new LinearInterpolator());
+                Animation animation= AnimationUtils.loadAnimation(MainActivity.this,R.anim.translateanim);
+                animation.setInterpolator(new CycleInterpolator(1));
+                tv.startAnimation(animation);
+            }
+        });
     }
 }
